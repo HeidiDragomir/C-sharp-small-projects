2090a1d45d34e1b3f8448f6c8bf4fb5e82e31e5a
HeidiDragomir <marinela.dragomir@chasacademy.se>
2023-07-14 14:11:02 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Numerics;
+
+Console.WriteLine("Hello");
+Console.WriteLine("Input the first number:");
+var userInputFirstNumber = Console.ReadLine();
+int firstNumber = int.Parse(userInputFirstNumber);
+
+Console.WriteLine("Input the second number:");
+var userInputSecondNumber = Console.ReadLine();
+int secondNumber = int.Parse(userInputSecondNumber);
+
+
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+var userInputSelectedOption = Console.ReadLine();
+
+
+
+if (CompareCaseSensitive(userInputSelectedOption, "A"))
+{
+    var sum = firstNumber + secondNumber;
+    PrintCalculatedEquation(firstNumber, secondNumber, "+", sum);
+}
+else if (CompareCaseSensitive(userInputSelectedOption, "S"))
+{
+    var substraction = firstNumber - secondNumber;
+    PrintCalculatedEquation(firstNumber, secondNumber, "-", substraction);
+}
+else if (CompareCaseSensitive(userInputSelectedOption, "M"))
+{
+    var multiplication = firstNumber * secondNumber;
+    PrintCalculatedEquation(firstNumber, secondNumber, "*", multiplication);
+}
+else 
+{
+    Console.WriteLine("Invalid option!");
+}
+
+Console.WriteLine("Press any key to close.");
+
+void PrintCalculatedEquation (int number1, int number2, string @operator, int operation)
+{
+    Console.WriteLine(number1 + " " + @operator + " " + number2 + " = " + operation);
+}
+
+bool CompareCaseSensitive (string left, string right)
+{
+    return left.ToUpper() == right.ToUpper();
+}
+
+Console.ReadKey();
---
+++ TodoList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+
+Console.WriteLine("Please provide a number: ");
+
+string userInput = Console.ReadLine();
+
+int number = int.Parse(userInput);
+
+Console.WriteLine("The number is: " + number);
+
+Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine();
+bool isLong = IsLong(userChoice);
+Console.WriteLine(isLong);
+
+
+var result = Concatenat("Heidi", 13);
+Console.WriteLine("My name is " + result);
+
+if (userChoice == "S")
+{
+    PrintSelectedChoice("See all TODOs");
+}
+
+else if (userChoice == "A")
+{
+    PrintSelectedChoice("Add a TODO");
+}
+
+else if (userChoice == "R")
+{
+    PrintSelectedChoice("Remove a TODO");
+}
+
+else if (userChoice == "E")
+{
+    PrintSelectedChoice("Exit");
+}
+
+void PrintSelectedChoice(string selectedChoice)
+{
+    Console.WriteLine("Selected Option: " + selectedChoice);
+}
+
+Console.WriteLine(PrintSelectedChoice("ORICE"));
+
+bool IsLong(string input)
+{
+    return input.Length > 10;
+
+}
+
+string Concatenat(string text1, int a)
+{
+    return text1 + a;
+}
+
+Console.ReadKey();
